58127587535a07144b457c907310f330f0544353
Mukiev Mukhammad <madani200160@gmail.com>
2018-09-18 16:58:16 +0300

    Задачи с пвышенным баллом

---
+++ 2018/FALL/PR/BankPercent/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankPercent
+{
+    class Program
+    {
+        // Автор Мукиев Мухаммад
+        // Пользователь должен ввести исходные данные с консоли — три числа через пробел:
+        //исходную сумму, процентную ставку (в процентах) и срок вклада в месяцах.
+        //Программа должна вывести накопившуюся сумму на момент окончания вклада.
+
+        public static double Calculate(string userInput)
+        {
+            //Разделим строку на построки
+            string[] s = userInput.Split(' ');
+            // p - это сумма вклада
+            double p = double.Parse(s[0]);
+            // r - коэффицент = 1 + процент/100
+            double r = double.Parse(s[1]) / 1200 +1 ;
+            // k - количество месяцев
+            double k = double.Parse(s[2]);
+            // sum - это сумма счета на момент закрытия
+            double sum = p * Math.Pow(r, k);
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            /*// sum is quantity of money
+            double sum = double.Parse(Console.ReadLine());
+            // percent is annual percent
+            double percent = double.Parse(Console.ReadLine());
+            // month is quantity of month of account
+            double month = double.Parse(Console.ReadLine());*/
+
+            
+            Console.WriteLine(Calculate(Console.ReadLine()));
+
+        }
+    }
+}
