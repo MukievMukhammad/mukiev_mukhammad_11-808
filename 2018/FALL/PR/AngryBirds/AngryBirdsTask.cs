84481b980b2d44ed85222bc82ee81e353863bb4d
Mukiev Mukhammad <madani200160@gmail.com>
2018-09-18 08:10:29 +0300

    Задачи с повышенными баллами

---
+++ 2018/FALL/PR/AngryBirds/AngryBirdsTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+        //Это простой симулятор системы прицеливания.
+        //В файле AngryBirdsTask реализуйте функцию расчета угла прицеливания, 
+        //в зависимости от начальной скорости снаряда и дальности до цели. 
+        // Автор Мукиев Мухаммад
+
+        //  Ниже — это XML документация, её использует ваша среда разработки, 
+        // чтобы показывать подсказки по использованию методов. 
+        // Но писать её естественно не обязательно.
+        /// <param name="v">Начальная скорость</param>
+        /// <param name="distance">Расстояние до цели</param>
+        /// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+        public static double FindSightAngle(double v, double distance)
+		{
+            double angle = Math.Asin((distance * 9.8) / (v * v))/2;
+            if ((distance / v) < ((v * Math.Sin(angle)) / 9.8))
+            {
+                return 0;
+            }
+            else
+            {
+                return angle;
+            }
+		}
+	}
+}
---
+++ 2018/FALL/PR/Bilyard/BilliardsTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        //Реализуйте метод для расчета угла отскока шарика от стены. 
+        //Считайте, что угол падения равен углу отражения, 
+        //то есть можно пренебречь всеми физическими эффектами,
+        //связанными с кручением шаров, трением шара об стенку и т.п.
+        //Автор Мукиев Мухаммад
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="directionRadians">Угол направелния движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns></returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            //TODO
+            return 2*wallInclinationRadians-directionRadians;
+        }
+    }
+}
