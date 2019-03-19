1e43553f04cbf04730ca4a686ef5607c941ba2cf
Mukiev Mukhammad <madani200160@gmail.com>
2019-03-19 04:59:58 +0300

    Semestrovka

---
+++ 2019/Sprin/SEM/IntroSort.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntroSortOnArray
+{
+    public static class HeapSort
+    {
+        public static int Iteration;
+        public static void Sort(int[] array, int iteration)
+        {
+            Iteration = iteration
+            Heap_sort(array, array.Length);
+        }
+
+        public static void Heapify(int[] arr, int pos, int size)
+        {
+            int temp;
+            while (2 * pos + 1 < size)
+            {
+                int t = 2 * pos + 1;
+                if (2 * pos + 2 < size && arr[2 * pos + 2] >= arr[t])
+                {
+                    t = 2 * pos + 2;
+                }
+                if (arr[pos] < arr[t])
+                {
+                    temp = arr[pos];
+                    arr[pos] = arr[t];
+                    arr[t] = temp;
+                    pos = t;
+                }
+                else break;
+            }
+        }
+
+        public static void HeapMake(int[] arr, int arrLength)
+        {
+            for (int i = arrLength - 1; i >= 0; i--)
+            {
+                Heapify(arr, i, arrLength);
+            }
+        }
+
+        public static void Heap_sort(int[] arr, int arrLength)
+        {
+            int temp;
+            HeapMake(arr, arrLength);
+            while (arrLength > 0)
+            {
+                temp = arr[0];
+                arr[0] = arr[arrLength - 1];
+                arr[arrLength - 1] = temp;
+                arrLength--;
+                Heapify(arr, 0, arrLength);
+            }
+        }
+    }
+
+    public static class IntroSort
+    {
+        public static int Iteration;
+
+        public static void Sort(int[] array)
+        {
+            var dipthLimit = 2 * Math.Log(array.Length);
+            QuickSort(array, 0, array.Length - 1, (int)dipthLimit);
+        }
+
+        static int[] QuickSort(int[] array, int left, int right, int dipthLimit)
+        {
+            Iteration++;
+            if(dipthLimit == 0)
+            {
+                HeapSort.Sort(array);
+                return array;
+            }
+
+            if (left < right)
+            {
+                var p = QuickSortPatrition(array, left, right);
+                QuickSort(array, left, p, dipthLimit - 1);
+                QuickSort(array, p + 1, right, dipthLimit - 1);
+            }
+            return array;
+        }
+
+        static int QuickSortPatrition(int[] array, int left, int right)
+        {
+            var pivot = array[left];
+            var i = left - 1;
+            var j = right + 1;
+            while (true)
+            {
+                do
+                    j--;
+                while (array[j] > pivot);
+                do
+                    i++;
+                while (array[i] < pivot);
+
+                if (i < j)
+                {
+                    var t = array[i];
+                    array[i] = array[j];
+                    array[j] = t;
+                }
+                else return j;
+            }
+        }
+    }
+
+}
---
+++ 2019/Sprin/SEM/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using IntroSortOnArray;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Semestrovka
+{
+    class Program
+    {
+        static Random random = new Random();
+
+        static int[] GenerateRandomArray(int length)
+        {
+            var array = new int[length];
+            for (int i = 1; i < array.Length; i++)
+                array[i] = random.Next();
+            return array;
+        }
+
+        private static Chart MakeChart(Series introSort, Series line)
+        {
+            var chart = new Chart();
+            chart.ChartAreas.Add(new ChartArea());
+
+            introSort.ChartType = SeriesChartType.FastLine;
+            introSort.Color = Color.Green;
+            introSort.BorderWidth = 3;
+
+            line.ChartType = SeriesChartType.FastLine;
+            line.Color = Color.Red;
+            line.BorderWidth = 3;
+
+            chart.Series.Add(introSort);
+            // chart.Series.Add(line);
+            chart.Dock = DockStyle.Fill;
+            return chart;
+        }
+
+        static void MeasureTime(int[] array, Action<int[]> sort, Series series)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            sort(array);
+            watch.Stop();
+            series.Points.Add(new DataPoint(array.Length, (float)watch.ElapsedMilliseconds / 1000));
+        }
+
+        public static void Main()
+        {
+            var arraySort = new Series();
+            var line = new Series();
+
+            for (int i = 100; i <= 1000000; i *= 2)
+            {
+                GC.Collect();
+                var array = GenerateRandomArray(i);
+                MeasureTime(array, IntroSort.Sort, arraySort);
+                line.Points.Add(new DataPoint(i, Math.Log(i)));
+            }
+
+            var chart = MakeChart(arraySort, line);
+            var form = new Form();
+            form.ClientSize = new Size(800, 600);
+            form.Controls.Add(chart);
+            Application.Run(form);
+        }
+    }
+}
