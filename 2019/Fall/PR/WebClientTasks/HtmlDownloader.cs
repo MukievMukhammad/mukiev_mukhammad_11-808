d9eff0eba4172ae781e9875b4d7171797c4d521e
Mukiev Mukhammad <madani200160@gmail.com>
2019-09-29 13:48:20 +0300

    Делаем задание со скачиванием изображения и html страниц, которые расположены по указанному url адресу

---
+++ 2019/Fall/PR/WebClientTasks/HtmlDownloader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class HtmlDownloader
+{
+    // Скачивает html по url и парсит по тегам <a></a>, 
+    // извлекая ссылки на другие html страницы по атрибуту href=""
+    // Далее скачивает эти страницы
+    public static void DowloadHtlmPages(string url)
+    {
+        string htmlCode;
+        using (WebClient client = new WebClient())
+        {
+            htmlCode = client.DownloadString(url);
+            var pages = HrefParseHtml(htmlCode);
+            pages.Add(url);
+            var count = 0;
+            foreach (var page in pages)
+            {
+                client.DownloadFile(page, count + ".html");
+                count++;
+            }
+        }
+    }
+
+    // Парсит html по тегу <a></a> и атрибуду href=""
+    private static List<string> HrefParseHtml(string htmlCode)
+    {
+        var pages = new List<string>();
+        bool aTag = false;
+        for (int i = 0; i < htmlCode.Length - 3; i++)
+        {
+            if (!aTag && htmlCode.Substring(i, 3) == "<a ")
+            {
+                aTag = true;
+                i += 2;
+                continue;
+            }
+            if (aTag && htmlCode.Substring(i, 5) == "href=")
+            {
+                aTag = false;
+                int index = i + 6;
+                while (htmlCode[index] != '"')
+                    index++;
+                pages.Add(htmlCode.Substring(i + 6, index - i - 6));
+                i = index;
+            }
+        }
+        return pages;
+    }
+}
---
+++ 2019/Fall/PR/WebClientTasks/WebImage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class WebImage
+{
+    // Скачивает html по url и парсит по тегам <img>, извлекая ссылку на источник изображения
+    // Далее скачивает изображения последовательно
+    public static void DowloadImagesFrom(string url)
+    {
+        string htmlCode;
+        using (WebClient client = new WebClient())
+        {
+            htmlCode = client.DownloadString(url);
+            var images = ImgParseHtml(htmlCode);
+            var count = 0;
+            foreach (var page in pages)
+            {
+            	client.DownloadFile(page, count + ".img");
+                count++;
+            }
+        }
+    }
+
+    // Парсит html по тегу <img> и атрибуду src=" "
+    private static List<string> ImgParseHtml(string htmlCode)
+    {
+        var images = new List<string>();
+        bool img = false;
+        for (int i = 0; i < htmlCode.Length - 5; i++)
+        {
+            if (!img && htmlCode.Substring(i, 5) == "<img ")
+            {
+                img = true;
+                i += 4;
+                continue;
+            }
+            if (img && htmlCode.Substring(i, 4) == "src=")
+            {
+                img = false;
+                int index = i + 5;
+                while (htmlCode[index] != '"')
+                    index++;
+                images.Add(htmlCode.Substring(i + 5, index - i - 5));
+                i = index;
+            }
+        }
+        return images;
+    }
+}
