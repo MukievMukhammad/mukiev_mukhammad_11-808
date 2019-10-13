6a7647941d22aac9076e1fcf298a4d1746bf9699
Mukiev Mukhammad <madani200160@gmail.com>
2019-10-13 12:16:08 +0300

    Ulern делаем фильтр серого

---
+++ 2019/Fall/Uleran/Photoshop/Data/Pixle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop
+{
+    public struct Pixle
+    {
+        public Pixle(double r, double g, double b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        private double r;
+        public double R
+        {
+            get { return r; }
+            set
+            {
+                r = Check(value);
+            }
+        }
+        private double g;
+        public double G
+        {
+            get { return g; }
+            set
+            {
+                g = Check(value);
+            }
+        }
+        private double b;
+        public double B
+        {
+            get { return b; }
+            set
+            {
+                b = Check(value);
+            }
+        }
+
+        public double Check(double value)
+        {
+            if (value < 0 || value > 255) throw new ArgumentException();
+            return value;
+        }
+
+        public static Pixle operator *(Pixle p, double c)
+        {
+            return new Pixle(
+                        p.R * c,
+                        p.G * c,
+                        p.B * c);
+        }
+
+        public static Pixle operator *(double c, Pixle p)
+        {
+            return p * c;
+        }
+    }
+}
