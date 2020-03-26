806ece9c8a6c177914c9bf113368870b7a6111d6
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-03-26 15:22:20 +0300

    Update AuthFilter.cs

--- 2020/Sprin/HomeWork/Excercise3/SocialMedia/SocialMedia/Filter/AuthFilter.cs
+++ 2020/Sprin/HomeWork/Excercise3/SocialMedia/SocialMedia/Filter/AuthFilter.cs
@@ -1,22 +1,40 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using SocialMedia.Data;
 
 namespace SocialMedia.Filters
 {
     public class AuthFilter : Attribute, IAuthorizationFilter
     {
+        private UsersContext _usersContext = null;
         public AuthFilter()
         {
         }
 
         public void OnAuthorization(AuthorizationFilterContext context)
         {
+            _usersContext = (UsersContext)context.HttpContext.RequestServices.GetService(typeof(UsersContext));
+
             var token = context.HttpContext.Request.Cookies["token"];
-            if (token == null || token == "")
+            var id = context.HttpContext.Request.Cookies["id"];
+
+            if (string.IsNullOrEmpty(token))
             {
                 context.Result = new RedirectResult("/Account/Login");
             }
+            if (!IsAuth(double.Parse(token), int.Parse(id), context))
+                context.Result = new BadRequestResult();
+        }
+
+        private bool IsAuth(double token, int id, AuthorizationFilterContext context)
+        {
+            var user = _usersContext.Users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+                context.Result = new NotFoundResult();
+
+            return token == MyHashCode.GetHash(user.Email + user.Password);
         }
     }
 }
