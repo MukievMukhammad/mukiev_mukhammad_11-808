463af6583c5414479478842118f4c818a370cb3b
MukievMukhammad <42972450+MukievMukhammad@users.noreply.github.com>
2020-03-26 15:18:39 +0300

    AuthFilter

---
+++ 2020/Sprin/HomeWork/Excercise3/SocialMedia/SocialMedia/Filter/AuthFilter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace SocialMedia.Filters
+{
+    public class AuthFilter : Attribute, IAuthorizationFilter
+    {
+        public AuthFilter()
+        {
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            var token = context.HttpContext.Request.Cookies["token"];
+            if (token == null || token == "")
+            {
+                context.Result = new RedirectResult("/Account/Login");
+            }
+        }
+    }
+}
